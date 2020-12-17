e8153652390670cf4fc708a5beb1875c194bd99f
bloom-dream36 <dream.2001.samu@icloud.com>
2020-12-17 17:31:20 +0900

    電話番号バリテーション　修正

--- app/models/address_order.rb
+++ app/models/address_order.rb
@@ -6,7 +6,7 @@ class AddressOrder
   with_options presence: true do
     validates  :city, :house_number, :token, :item_id, :user_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "in invalid. Incluide hyphen(-)"}
-    validates :tell, numericality: { only_integer: true,message: "半角数字で入力してください"} ,format: { with: /\A\d{11}\z/, message: "is out of setting"}
+    validates :tell, numericality: { only_integer: true,message: "半角数字で入力してください"} ,format: { with: /\A\d{10}$|\d{11}\z/, message: "is out of setting"}
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
   
